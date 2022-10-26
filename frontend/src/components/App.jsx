8a05ac31f4ba73657b2886972f6bc19e6b96c81d
Ryan Schaefer <rschaefer1@me.com>
2022-10-26 10:53:24 -0500

    Start react routing

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,7 +1,14 @@
 import { Login } from "./pages/index";
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 export const App = () => (
   <>
-    <Login />
+    <Router>
+      {/* <Layout account = {account} setAccount={setAccountValue}/> */}
+      <Routes>
+        {/* <Route path="/" element={<Main setProduct={setProduct}/>} ></Route> */}
+        <Route path="/login" element={<Login/>} ></Route>
+      </Routes>
+    </Router>
   </>
 );
