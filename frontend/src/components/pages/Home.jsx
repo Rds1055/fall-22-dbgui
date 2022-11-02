f889a3075cfe5fa21bfa111e07df652beafb962a
mswigart <91754317+mswigart@users.noreply.github.com>
2022-11-02 15:48:51 -0500

    Created NavBar, Homepage, and Create Theory

--- frontend/src/components/pages/Home.jsx
+++ frontend/src/components/pages/Home.jsx
@@ -1,25 +1,19 @@
-import {useNavigate} from 'react-router-dom';
-
 export const Home = () => {
-    const navigate = useNavigate();
-    return (
+
+     return (
         <>
-            <button
-                type = "button"
-                onClick = {() => {
-                    navigate("/login");
-                }}
-            >
-                Login
-            </button>
-            <button
-                type = "button"
-                onClick = {() => {
-                    navigate("/register");
-                }}
-            >
-                Register
-            </button>
+        <div className = "pageStyle">
+            <div>
+                <h1> Today's Top Theory</h1>
+            </div>
+
+            <div className = "homeImage">
+            <img
+        src="https://www.goodtherapy.org/blog/blog/wp-content/uploads/2012/10/thought-bubbles-and-brain-illustration.jpg"
+        alt="car"
+      />
+            </div>
+        </div>
         </>
-    )
+     )
 }
---
+++ frontend/src/components/pages/Navbar.jsx
@@ -0,0 +1,35 @@
+export const Navbar = () => {
+    const path = window.location.pathname
+    return(
+    // <div className = "container">
+        <nav className="nav">
+
+            <a href="/" className = "site-title">
+            Home
+            </a>
+                <ul>
+                    <CustomLink href ="/CreateTheory">Create Theory</CustomLink>
+                
+                    <CustomLink href ="/Search">Search</CustomLink>
+                    
+                    <CustomLink href ="/Profile">Profile</CustomLink>
+                    
+                    <CustomLink href ="/SubmitTitle">Submit Title</CustomLink>
+                    
+                    <CustomLink href ="/LoginSignup">Login Signup</CustomLink>
+                
+                </ul>
+            </nav>
+    // </div>
+    
+    )
+}
+
+function CustomLink({href,children, ...props}){
+    const path = window.location.pathname
+    return (
+    <li className={path === href ? "active" : ""}>
+        <a href={href}{...props}>{children}</a>
+    </li>
+    )
+}
