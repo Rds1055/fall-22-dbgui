b279e1193bde44c580b9216b84d498c4def3f63f
Ryan Schaefer <rschaefer1@me.com>
2022-11-02 16:46:08 -0500

    Created components

--- frontend/src/components/common/TextAreaField.jsx
+++ frontend/src/components/common/TextAreaField.jsx
@@ -1,8 +1,7 @@
-export const TextField = ({ label, value, setValue }) => <>
+export const TextAreaField = ({ label, value, setValue }) => <>
     <div className="form-group mb-3">
         <label htmlFor="value">{ label }</label>
-        <input type="text"
-            name="value"
+        <textarea name="value"
             id="value"
             className="form-control"
             value={value}
