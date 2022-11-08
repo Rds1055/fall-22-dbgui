ff28f06d2deb22b91c200870b48595dbcf54ec39
MichaelLCastle <mlcastle@smu.edu>
2022-11-08 08:21:32 -0600

    start of advanced search

--- frontend/src/components/pages/Navbar.jsx
+++ frontend/src/components/pages/Navbar.jsx
@@ -6,7 +6,7 @@ export const Navbar = ()=> {
 
     
         <a class="ripple" href="./">
-        <img alt="example" class="img-fluid rounded-circle"src="https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/11-hands-on-crystal-ball-and-cryptocurrency-allan-swart.jpg" width="80" height="75" alt=""/>
+        <img alt="example" class="img-fluid rounded-circle"src="https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/11-hands-on-crystal-ball-and-cryptocurrency-allan-swart.jpg" width="80" height="75"/>
     </a>
     
 
