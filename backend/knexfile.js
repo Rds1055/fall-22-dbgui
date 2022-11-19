84841dd67b334229a8de8799eda4902c64a3b80a
Ryan Schaefer <rschaefer1@me.com>
2022-11-19 15:06:35 -0600

    Connect to rds with knex

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -20,7 +20,7 @@ module.exports = {
   client: 'mysql',
   debug: true,
   connection: {
-  host : process.env.MYSQL_DB_URL,
+  host : process.env.MYSQL_CLOUD_HOST,
   port : process.env.MYSQL_PORT,
   user : process.env.MYSQL_CLOUD_USER,
   password : process.env.MYSQL_CLOUD_PASS,
