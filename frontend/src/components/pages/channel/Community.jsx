e8eb529934c04d3f7c74c7e7d87b16b14d84d345
Ryan Schaefer <rschaefer1@me.com>
2022-11-21 23:07:49 -0600

    Working apis

--- frontend/src/components/pages/channel/Community.jsx
+++ frontend/src/components/pages/channel/Community.jsx
@@ -42,12 +42,8 @@ export const Community = () => {
     useEffect(() => {
         getChannelById(params.channel_id).then(x => setChannel(x[0]));
         getPostsByChannel(params.channel_id).then( x => setPosts(x));
-        console.log(channel.title)
-        console.log(channel.posts)
     }, []);
     
-
-
     const channel1 = new Channel(0,"Spider Man 3","Aug 1, 1943","Peter Parker (Tobey Maguire) and M.J. (Kirsten Dunst) seem to finally be on the right track in their complicated relationship, but trouble looms for the superhero and his lover. Peter's Spider-Man suit turns black and takes control of him, not only giving Peter enhanced power but also bringing out the dark side of his personality. Peter must overcome the suit's influence as two supervillains, Sandman and Venom, rise up to destroy him and all those he holds dear.");
     const postsW = [
         new Post(1,0,"Who dies soon?",20,"aug 2,2022","@marvin23"),
@@ -102,7 +98,7 @@ return(<>
     <div className='w-75 mx-auto rounded-3 mt-3'>
         <div className="card text-center">
             <div className="card-body col">
-                <h2 className="card-title">{channel.channel_title}</h2>
+                <h2 className="card-title">{channel.title}</h2>
                 <div className=' px-4 pt-1 m-3'>
                         <span className="m-4 fs-6 card-text">{channel.summary}</span>
                 </div>
