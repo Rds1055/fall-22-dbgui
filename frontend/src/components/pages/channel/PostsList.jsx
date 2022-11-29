8fe7f50652e01d3f1b08891bdbdca15c3231d16e
Ryan Schaefer <rschaefer1@me.com>
2022-11-29 14:42:09 -0600

    Fix channels disappearing

--- frontend/src/components/pages/channel/PostsList.jsx
+++ frontend/src/components/pages/channel/PostsList.jsx
@@ -9,7 +9,7 @@ export const PostsList = ({posts}) => {
     
     
    console.log(posts);
-if(posts.length==0){
+if(!posts || posts.length==0){
     return <> 
         <div className='card w-50 mx-auto text-center p-4 m-4'>
             <div className='card-title'>
