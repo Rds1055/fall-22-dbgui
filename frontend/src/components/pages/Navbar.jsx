16d63ffacfb8f68ba4520cba8a2809cb513da956
Ryan Schaefer <rschaefer1@me.com>
2022-11-28 23:44:08 -0600

    Working hamburger menu

--- frontend/src/components/pages/Navbar.jsx
+++ frontend/src/components/pages/Navbar.jsx
@@ -89,6 +89,7 @@ export const Navbar = ()=> {
 </nav>
 
 </div>
+
 <div className = "navigationBar small-view">
 <nav className="navbar navbar-expand-lg navbar navbar-dark bg-primary" fill="currentColor" >
     <div className = "container-fluid">
@@ -99,21 +100,69 @@ export const Navbar = ()=> {
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled">Disabled</a>
-        </li>
-      </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
+    <ul className="navbar-nav nav-fill w-100">
+
+<li className="nav-item">
+<button type="button" className="btn btn-outline-primary btn-lg btn-block">
+<a className="nav-link" href="/dashboard">Dashboard</a>
+</button>
+</li>
+
+<li className="nav-item">
+<button type="button" className="btn btn-outline-primary btn-lg btn-block">
+<a className="nav-link" href="/profile">Profile</a>
+</button>
+</li>
+
+<li className="nav-item">
+<button type="button" className="btn btn-outline-primary btn-lg btn-block">
+<a className="nav-link" href="SubmitTitle">Submit Title</a>
+</button>
+</li>
+
+{
+    sessionStorage.token &&
+    <li className="nav-item">
+    <button type="button" className="btn btn-outline-primary btn-lg btn-block">
+    <a href="#" className = "nav-link" 
+        onClick={ () => {
+            delete sessionStorage.token;
+            navigate("/");
+        } 
+    }>Logout</a>
+    </button>
+    </li>
+}
+
+{
+    !sessionStorage.token &&
+    <li className="nav-item">
+    <button type="button" className="btn btn-outline-primary btn-lg btn-block">
+    <a className="nav-link" href="/login">Login</a>
+    </button>
+    </li>
+}
+
+
+<li className="nav-item">
+<form className = "d-flex align-items-center justify-content-center">
+    <SearchField value = { search } setValue = { setSearch } placeholder = "Search"/>
+    <button type="button" className="btn btn-primary" onClick={() => {
+            if (!search) {
+                navigate(`/channel-search/ `);
+            } else {
+                navigate(`/channel-search/${ search }`);
+            }
+        }}>
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
+        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+        </svg>
+        <i className="fas fa-search"></i>
+    </button>
+</form>
+</li>
+
+</ul>
     </div>
     </div>
     
