2b0cdc2e4ec02adf11cb9d37ef764d299c05b2c5
Ryan Schaefer <rschaefer1@me.com>
2022-11-29 14:20:19 -0600

    Update comment

--- backend/models/comment.js
+++ backend/models/comment.js
@@ -20,10 +20,18 @@ const fetchCommentsById = async (comment_id) => {
     const results = await query;
     return results;
 }
-const updateComment = async (contents, comment_id)  => {
-    const query = knex(COMMENTS_TABLE).update({contents}).where({comment_id});
-    const results = await query;
-    return results;
+const updateComment = async (comment_id, comment)  => {
+    if (comment.contents) {
+        const contents = comment.contents;
+        const query = await knex(COMMENTS_TABLE).update({contents}).where({comment_id});
+    }
+    if (comment.likes) {
+        const likes = comment.likes;
+        const query = await knex(COMMENTS_TABLE).update({likes}).where({comment_id});
+    }
+    const sql = knex(COMMENTS_TABLE).where({ comment_id });
+    const result = await sql;
+    return result;
 }
 const createComment = async (body) => {
     const query = knex(COMMENTS_TABLE).insert({...body});
