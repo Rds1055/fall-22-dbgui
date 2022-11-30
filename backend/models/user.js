d989fb7756fdbc12fb4f632acacaf6b6e6762093
Ryan Schaefer <rschaefer1@me.com>
2022-11-30 11:16:23 -0600

    Add get posts and likes for users

--- backend/models/user.js
+++ backend/models/user.js
@@ -12,6 +12,34 @@ const fetchUsersByName = async (username) => {
     const results = await query;
     return results;
 }
+const fetchUserPosts = async (username) => {
+    const query = knex(USERS_TABLE)
+        .join("posts", "users.username", "posts.user")
+        .count("*").where({ "users.username": username });
+    const result = await query;
+    return result;
+}
+const fetchUserPostLikes = async (username) => {
+    const query = knex(USERS_TABLE)
+        .join("posts", "users.username", "posts.user")
+        .sum("posts.likes").where({ "users.username": username });
+    const result = await query;
+    return result;
+}
+const fetchUserComments = async (username) => {
+    const query = knex(USERS_TABLE)
+        .join("comments", "users.username", "comments.user")
+        .count("*").where({ "users.username": username });
+    const result = await query;
+    return result;
+}
+const fetchUserCommentLikes = async (username) => {
+    const query = knex(USERS_TABLE)
+        .join("comments", "users.username", "comments.user")
+        .sum("comments.likes").where({ "users.username": username });
+    const result = await query;
+    return result;
+}
 const updateUsername = async (username, user_id) => {
     const query = knex(USERS_TABLE).update({username}).where({user_id});
     const results = await query;
@@ -60,6 +88,10 @@ const authenticateUser = async (username, pword) => {
    module.exports = {
     fetchAllUsers,
     fetchUsersByName,
+    fetchUserPosts,
+    fetchUserPostLikes,
+    fetchUserComments,
+    fetchUserCommentLikes,
     createUser,
     updateUsername,
     updatePassword,
